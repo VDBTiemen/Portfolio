7b9cfcdd77f4d6d032082ab6aa69253ee73318d8
Tiemen <tiemen.vandenbergh@student.kdg.be>
2024-12-11 17:24:48 +0100

    form

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,13 @@
-document.getElementById("map").innerHTML = map;
-var map = L.map("map").setView([51.213487098383204, 4.25898036090494], 13);
-var marker = L.marker([51.213487098383204, 4.25898036090494]).addTo(map);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+window.addEventListener("load", init);
 
+function init() {
+  document.getElementById("map").innerHTML = "map";
+  
+  let map = L.map("map").setView([51.213487098383204, 4.25898036090494], 13);
+  let marker = L.marker([51.213487098383204, 4.25898036090494]).addTo(map);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+}
